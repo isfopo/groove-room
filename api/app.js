4be2f650d6c2fab353696c248bf468df8865b918
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-26 11:45:56 -0500

    removes spotify auth api from api

--- api/app.js
+++ api/app.js
@@ -9,18 +9,16 @@ require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+
 app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
