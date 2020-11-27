14e0071d8a049915b10d5f8b412aed3cd3454da8
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-27 00:54:47 -0500

    adds post request to add user

--- client-web/src/components/Home.js
+++ client-web/src/components/Home.js
@@ -1,23 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import { useCookies } from 'react-cookie';
-
 import { Login } from './Login.js';
+
+const SpotifyWebApi = require('spotify-web-api-js');
 
 export const Home = () => {
 
     const [cookies, setCookie, removeCookie] = useCookies(['authorization-cookie']);
 
-    const [token, setToken] = useState(cookies.spotify_auth)
+    const [token, setToken] = useState(cookies.spotify_auth.access_token)
     
     useEffect(() => {
-        console.log(token)
-    }, [])
+        const spotifyApi = new SpotifyWebApi();
+        spotifyApi.setAccessToken(token);
+        spotifyApi.getMe()
+            .then( async (data) => {
+                fetch('http://localhost:3001/users', {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    method: "POST",
+                    body: JSON.stringify(data)
+                })
+            },
+            function (err) {
+                console.error(err);
+            }
+          );
+    }, [token])
+
+    const playSpotify = () => {
+
+    }
 
 
     return (
         <div>
-            <h1>Home</h1>
             <Login />
+            <button onClick={playSpotify}>Play</button>
         </div>
     )
 }
