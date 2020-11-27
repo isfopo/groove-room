0dc17719dcd54ba7b170147e52cf1dd28d37af94
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-27 18:22:11 -0500

    adds add room view

--- client-web/src/components/AddRoom.js
+++ client-web/src/components/AddRoom.js
@@ -1,43 +1,86 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { useCookies } from 'react-cookie';
 
 import back from '../icons/arrow_back_ios-24px.svg';
+import copy from '../icons/content_copy-24px.svg';
+import done from '../icons/done-24px.svg';
 
 export const AddRoom = (props) => {
     
-    const [name, setName] = useState("");
+    const [roomName, setRoomName] = useState("");
+    const [roomId, setRoomId] = useState('');
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [copied, setCopied] = useState(false);
+
+    const [cookies, setCookie, removeCookie] = useCookies();
+
+    const textAreaRef = useRef(null);
+  
+    function copyToClipboard(e) {
+      textAreaRef.current.select();
+      document.execCommand('copy');
+      e.target.focus();
+      setCopied(true);
+      setTimeout(() => {
+        props.history.push('/');
+      }, 3000)
+    };
     
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        setIsSubmitted(true)
-        // use name state because it was already set
-        // post new room in database
-
-    }
-    
-    const goBack = () => {
-        props.history.push('/');
+        setIsSubmitted(true);
+        fetch('http://localhost:3001/rooms/create', {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                room_name: roomName,
+                creator_id: cookies.user.id
+            })
+        })
+            .then(res => res.json())
+            .then(res => setRoomId(res.id))
     }
 
     return (
         <div className="add-room">
-        { !isSubmitted ?
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Room Name:
-                    <input 
-                        type="text"
-                        value={name}
-                        onChange={e => setName(e.target.value)} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        :
-            <p>{name}</p>
-        }
-        <button onClick={goBack}>
-            <img src={back} alt="back"/>
-        </button>
+            { !isSubmitted ?
+                <form readOnly onSubmit={handleSubmit}>
+                    <label>
+                        Room Name:
+                        <input 
+                            type="text"
+                            value={roomName}
+                            onChange={e => setRoomName(e.target.value)} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            :
+                <div>
+                    {
+                    document.queryCommandSupported('copy') &&
+                        <div>
+                            <button onClick={copyToClipboard}>Copy</button> 
+                            { !copied ?
+                                <img src={copy} alt="copy"/>
+                            :
+                                <img src={done} alt="done"/>
+                            }
+                        </div>
+                    }
+                    <form >
+                        <textarea
+                        readOnly
+                        ref={textAreaRef}
+                        value={roomId}
+                        />
+                    </form>
+                </div>
+            }
+            <a href="/">
+                <img src={back} alt="back"/>
+            </a>
         </div>
     )
 }
