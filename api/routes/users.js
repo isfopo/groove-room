31b7f40282f468606ef44b3d885eb59e16003b7c
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-27 00:53:18 -0500

    config sequelize

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../db/models').User;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
   res.send('respond with a resource');
+});
+
+router.post('/', async (req, res) => {
+  const user = await User.findOrCreate({
+    where: {
+      name: req.body.display_name,
+      image: req.body.images[0].url
+    }
+  });
+  res.json(user);
 });
 
 module.exports = router;
