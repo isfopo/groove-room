97e77aa95a971d2d9f46d6718a18bf0877c0e852
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-27 18:21:06 -0500

    adds room routes

---
+++ api/routes/rooms.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var Room = require('../db/models').Room;
+
+/* GET profiles in room */
+router.get('/', async (req, res, next) => {
+  res.send('respond with a resource');
+});
+
+/* POST create new room */
+router.post('/create', async (req, res) => {
+    const room = await Room.create({
+        name: req.body.room_name,
+        skip_vote: 0
+    });
+    res.json(room);
+});
+
+module.exports = router;
