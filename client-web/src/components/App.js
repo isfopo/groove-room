fe5374ab10dfb8a0528e5cadf04a624396803548
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-27 09:52:14 -0500

    removes cookies

--- client-web/src/components/App.js
+++ client-web/src/components/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
+import { CookiesProvider, useCookies } from 'react-cookie';
 import { Home } from './Home.js'
 import {RedirectPage} from './RedirectPage.js';
 
 function App() {
+
+  const [cookies, setCookie, removeCookie] = useCookies(['authorization-cookie']);
+
   return (
     <div className="main">
       <CookiesProvider>
