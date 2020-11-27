b0836f87f64cf4ce0aa1e4ea45a44d01a73df57e
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-27 00:54:19 -0500

    remove Switch import

--- client-web/src/components/App.js
+++ client-web/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 import { Home } from './Home.js'
 import {RedirectPage} from './RedirectPage.js';
