af8c4a81c582bf30ac57fabb645bef9d6ae909d4
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-27 22:58:16 -0500

    adds icons to right sidebar

--- client-web/src/components/RightSidebar.js
+++ client-web/src/components/RightSidebar.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import add from '../icons/playlist_add-24px.svg';
+import skip from '../icons/skip_next-24px.svg';
+import back from '../icons/arrow_forward_ios-24px.svg';
+
+import '../styles/Sidebar.css';
+
 export const RightSidebar = () => {
     return (
-        <div className="right-sidebar">
-            
+        <div className="sidebar right">
+            <img src={ add } alt="add" />
+            <img src={ skip } alt="skip" />
+            <img src={ back } alt="back" />
         </div>
     )
 }
