720b68c4e947ae4e84cf2cab4445af4cc00744b1
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-27 22:58:37 -0500

    adds styles to left sidebar

--- client-web/src/components/LeftSidebar.js
+++ client-web/src/components/LeftSidebar.js
@@ -3,25 +3,24 @@ import { useCookies } from 'react-cookie';
 
 import add from '../icons/add-24px.svg';
 import join from '../icons/group-24px.svg';
+import back from '../icons/arrow_back_ios-24px.svg';
 
-import '../styles/LeftSidebar.css';
+import '../styles/Sidebar.css';
 
 export const LeftSidebar = (props) => {
 
-    // render all rooms a user in a part of "/users/rooms"
     const [activeRooms, setActiveRooms] = useState([]);
 
-    const [cookies, setCookie, removeCookie] = useCookies();
+    const [cookies] = useCookies();
 
     useEffect(() => {
-        // fetch all rooms that the user is a part of and return them as an array
         fetch(`http://localhost:3001/rooms/${cookies.user.id}`)
             .then(res => res.json())
             .then(res => setActiveRooms(res))
     }, [cookies.user.id])
 
     return (
-        <div className="left-sidebar">
+        <div className="sidebar left">
             <div className='room-display' >
                 {
                     activeRooms.map( room => 
@@ -32,6 +31,7 @@ export const LeftSidebar = (props) => {
                     )
                 }
             </div>
+            <img src={ back } alt="back" />
             <a href="/join-room">
                 <img src={ join } alt="join" />
             </a>
