be58a7793c9746b456699db5826ec064c34de0c0
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-29 18:48:01 -0500

    adds room line item component with menu

---
+++ client-web/src/components/RoomLineItem.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import queryString from 'query-string';
+
+import more from '../icons/more_vert-24px.svg'
+
+export const RoomLineItem = (props) => {
+
+    const [showMenu, setShowMenu] = useState(false);
+
+    return (
+        <div onMouseLeave={() => setShowMenu(false)}>
+            <p>{props.room.name}</p>
+            <img onMouseEnter={() => setShowMenu(true)} src={ more } alt="more" />
+
+            { showMenu &&
+                <div className='room-menu' >
+                    <a href={`/invite/${queryString.stringify(props.room)}`}>Invite</a><br />
+                    <a href={`/rename/${queryString.stringify(props.room)}`}>Rename</a><br />
+                    <a href={`/remove/${queryString.stringify(props.room)}`}>Remove</a>
+                </div>
+            }
+        </div>
+    )
+}
