a182299b849f899744f5218d92d34a2d91fcd737
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-30 11:52:04 -0500

    will create user

--- client-web/src/components/Home.js
+++ client-web/src/components/Home.js
@@ -16,22 +16,20 @@ export const Home = (props) => {
     const [currentRoom, setCurrentRoom] = useState({});
     
     useEffect(() => {
-        if (!cookies.user) {
-            const spotifyApi = new SpotifyWebApi();
-            spotifyApi.setAccessToken(token);
-            spotifyApi.getMe()
-                .then( async (data) => {
-                    fetch('http://localhost:3001/users', {
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        method: "POST",
-                        body: JSON.stringify(data)
-                    })
-                    .then(res => res.json())
-                    .then(res => setCookie("user", res[0]))
+        const spotifyApi = new SpotifyWebApi();
+        spotifyApi.setAccessToken(token);
+        spotifyApi.getMe()
+            .then( async (data) => {
+                fetch('http://localhost:3001/users', {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    method: "POST",
+                    body: JSON.stringify(data)
                 })
-        }
+                .then(res => res.json())
+                .then(res => setCookie("user", res[0]))
+            })
     }, [token])
 
     const handleLogOut = () => {
