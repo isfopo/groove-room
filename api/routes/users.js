86bf2d8aafc4321639a81045829d7505ceea96b0
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-30 11:12:05 -0500

    saves image url to user

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,18 +2,22 @@ var express = require('express');
 var router = express.Router();
 var User = require('../db/models').User;
 
-/* GET users listing. */
+// GET users listing
 router.get('/', async (req, res, next) => {
   res.send('respond with a resource');
 });
 
+// POST new user or return existing user
 router.post('/', async (req, res) => {
   const user = await User.findOrCreate({
     where: {
-      name: req.body.display_name,
-      image: req.body.images[0].url
+      name: req.body.display_name
     }
   });
+  
+  user[0].image = req.body.images[0].url;
+  await user[0].save();
+
   res.json(user);
 });
 
