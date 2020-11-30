b9a5ca69390a69497a946dc974c56add119aafab
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-30 11:12:28 -0500

    move cookie removal to Home

--- client-web/src/components/LogOut.js
+++ client-web/src/components/LogOut.js
@@ -3,14 +3,11 @@ import { useCookies } from 'react-cookie';
 
 export const LogOut = (props) => {
 
-    const [cookies, setCookie, removeCookie] = useCookies(["user"]);
-
     useEffect(() => {
-        removeCookie("user"); // not removing cookie
         setTimeout(() => {
             props.history.push('/')
         }, 3000);
-    }, [props.history, removeCookie])
+    }, [props.history])
 
     return (
         <div>
