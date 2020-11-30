bee0880b9dcde0ff6d8e41d8dcd4935ccf61ab7d
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-30 14:50:57 -0500

    adds delete route for profiles

--- api/routes/profiles.js
+++ api/routes/profiles.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var Room = require('../db/models').Room;
 var Profile = require('../db/models').Profile;
 
-// get Profiles in Room
-router.get('/:room_id', async(req, res) => {
+// GET Profiles in Room
+router.get('/:room_id', async (req, res) => {
     const profiles = await Profile.findAll({
         where: {
             room_id: req.params.room_id
@@ -12,6 +12,17 @@ router.get('/:room_id', async(req, res) => {
     })
 
     res.json(profiles)
+})
+
+// DELETE Profile
+router.delete('/delete', async (req, res) => {
+
+    const profileToDelete = await Profile.findByPk(req.body.profile_id);
+    await profileToDelete.destroy();
+
+    // TODO: if room is empty, delete that as well
+
+    res.json({status: 200})
 })
 
 module.exports = router;
