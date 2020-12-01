8d8f3f73437b38e9591fa43deba850d3f45af202
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-30 20:06:41 -0500

    removes unused route

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 var User = require('../db/models').User;
 
-// GET users listing
-router.get('/', async (req, res, next) => {
-  res.send('respond with a resource');
-});
-
 // POST new user or return existing user
 router.post('/', async (req, res) => {
   const user = await User.findOrCreate({
